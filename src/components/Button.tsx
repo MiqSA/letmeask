789c514eb991efa9b8d77e9cf37881a4c4e412a1
MiqSA <eng.miqueias.sa@gmail.com>
2021-06-23 19:32:31 -0300

    Estilos e Autenticação do usuário

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,12 @@
-type ButtonProps = {
-  text ?: string;
-}
+import { ButtonHTMLAttributes } from 'react' 
+
+import '../styles/button.scss'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
 export function Button(props: ButtonProps){
   return (
-    <button>{props.text || 'Default'}</button>
+    <button className="button" {...props}>  
+    </button>
   )
 }
